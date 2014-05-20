79c8eafe73e6802176a6822ca19321df5aa8fa8b
IR236 <igor.rajic236@gmail.com>
2014-05-20 19:13:29 +0200

    final

--- cachematrix.R
+++ cachematrix.R
@@ -5,14 +5,22 @@
 
 ## makeCacheMatrix creates new type of the matrix object which can be cached
 makeCacheMatrix <- function(x = matrix()) {
+        ## sets cache to NULL at initilization (init)
         ix <- NULL
+        ## set function used for after init set of the object x
         set <- function(y){
+                ## sets x in the environment of the makeCacheMatrix function
                 x <<- y
+                ## clears the cache 
                 ix <<- NULL
         }
+        ## returns the matrix set via init or set function
         get <- function() x
+        ## setc sets the cache
         setc <- function(s) ix <<- s
+        ## getc gets the cache
         getc <- function() ix
+        ##returns list of functions
         list(set = set, get = get, setc = setc,getc = getc)
         
 }
